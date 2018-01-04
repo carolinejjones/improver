cb339d4e6c4531b75b7b7ca18bc44a61654c9b0f
Caroline Jones <caroline.jones@metoffice.gov.uk>
2018-01-04 14:14:06 +0000

    Adding code to calculate day/night weather codes.

--- lib/improver/wxcode/wxcode_utilities.py
+++ lib/improver/wxcode/wxcode_utilities.py
@@ -32,6 +32,12 @@
 
 from collections import OrderedDict
 import numpy as np
+import math
+import cartopy.crs as ccrs
+
+from improver.utilities.temporal import iris_time_to_datetime
+from improver.utilities.spatial import lat_lon_determine
+
 
 _WX_DICT_IN = {0: 'Clear_Night',
                1: 'Sunny_Day',
@@ -66,6 +72,8 @@ _WX_DICT_IN = {0: 'Clear_Night',
                30: 'Thunder'}
 
 WX_DICT = OrderedDict(sorted(_WX_DICT_IN.items(), key=lambda t: t[0]))
+
+DAYNIGHT_CODES = [1, 3, 10, 14, 17, 20, 23, 26, 29]
 
 
 def add_wxcode_metadata(cube):
@@ -109,3 +117,133 @@ def expand_nested_lists(query, key):
         else:
             items.extend([item])
     return items
+
+
+def calc_day_number(day, month, year):
+    """
+    Calculate the day of the year.
+
+    Args:
+        day (int):
+            Day of the month.
+        month (int):
+            Integer value for month, January = 1, December = 12 etc.
+        year (int):
+            Year YYYY
+
+    Returns:
+        day_number:
+             Day of the year, 1 to 365 or 366 in a leap year.
+    """
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if year % 4 == 0:
+        if year % 100 != 0:
+            month_days[1] = 29
+        elif year % 400 != 0:
+            month_days[1] = 28
+        else:
+            month_days[1] = 29
+    day_number = 0
+    for i in range(0, month-1):
+        day_number += month_days[i]
+    day_number += day
+    return day_number
+
+
+def solar_pos(latitude, longitude, day_number, utc_hours):
+    """
+    Calculate solar elevation for a latitude and longitude
+    for a day of the year and an hour (in UTC)
+
+    Args:
+        latitude (float):
+            Latitude of the point.
+        longitude (float):
+            Longitude of the point.
+        day_number (int):
+            Day of the year.
+        utc_hour (float):
+            Hour of the day in UTC
+
+    Returns:
+        solar_elevation (float):
+            Solar elevation in radians
+
+    """
+    deg2rad = math.pi/180.0
+
+    thetao = 2*math.pi*day_number/365.0
+
+    eqt = (0.000075 + 0.001868 * math.cos(thetao) -
+           0.032077 * math.sin(thetao) - 0.014615 * math.cos(2*thetao) -
+           0.040849 * math.sin(2*thetao))
+    # Declination (degrees):
+    decl = -23.5 * math.cos((0.9856*day_number+9.3)*deg2rad)
+    # Longitudinal Correction from the Grenwich Meridian
+    lon_correction = 24.0*longitude/360.0
+    # Solar time (hours):
+    solar_time = utc_hours + lon_correction + eqt*12/math.pi
+    # Hour angle (degrees):
+    degree_hour = (solar_time - 12.0) * 15.0
+
+    # Calculate solar position:
+
+    solar_elevation = (math.asin(math.sin(decl * deg2rad) *
+                                 math.sin(latitude * deg2rad) +
+                                 math.cos(decl*deg2rad) *
+                                 math.cos(latitude*deg2rad) *
+                                 math.cos(degree_hour*deg2rad)))
+
+    return solar_elevation
+
+
+def night_wxcode(wxcode):
+    """
+    Update weather code from Day value to Night value
+
+    Args:
+        wxcode (int):
+            Weather symbol code.
+
+    Returns:
+        night_wxcode(int):
+            Night version of Weather symbol code.
+    """
+    night_wxcode = wxcode
+    if wxcode in DAYNIGHT_CODES:
+        night_wxcode = wxcode - 1
+    return night_wxcode
+
+
+def night_day_wxcode(wxcube):
+    """
+    Modify weather symbols within a cube to account for
+    whether it is night or day.
+
+    Please note this method is very slow
+
+    Args:
+        wxcube (iris.cube.Cube):
+            Weather code cube.
+
+    Returns:
+        updated_wxcube (iris.cube.Cube):
+            Updated Weather code cube.
+    """
+    updated_wxcube = wxcube
+    dtval = iris_time_to_datetime(wxcube.coord('time'))[0]
+    day_number = calc_day_number(dtval.day, dtval.month, dtval.year)
+    utc_hours = (dtval.hour * 60.0 + dtval.minute) / 60.0
+    trg_latlon = ccrs.PlateCarree()
+    trg_crs = lat_lon_determine(wxcube)
+    y_points = wxcube.coord('projection_y_coordinate').points
+    x_points = wxcube.coord('projection_x_coordinate').points
+    for i, yval in enumerate(y_points):
+        for j, xval in enumerate(x_points):
+            lon, lat = trg_latlon.transform_point(xval, yval, trg_crs)
+            solar_elevation = solar_pos(lat, lon, day_number, utc_hours)
+            sin_solar_elevation = math.sin(solar_elevation)
+            if sin_solar_elevation < 0.0:
+                updated_wxcube.data[i, j] = (
+                    night_wxcode(updated_wxcube.data[i, j]))
+    return updated_wxcube
